b2c18c98f8a214ae8a673675238ae2040ccc2ca1
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-08-27 16:47:00 +0500

    Updating css replace file

---
+++ replace-folder/replace-css.js
@@ -0,0 +1,36 @@
+filter "*.svg"
+replace "fill="#145DBF""
+with "fill=var(--sidebar-bg)"
+
+replace "fill="white""
+with "fill=var(--center-channel-bg)"
+
+replace "fill="#1153AB""
+with "fill=var(--sidebar-header-bg)"
+
+replace "fill="#06D6A0""
+with "fill=var(--online-indicator)"
+
+replace "fill="#FFBC42""
+with "fill=var(--away-indicator)"
+
+replace "fill="#F74343""
+with "fill=var(--dnd-indicator)"
+
+replace "fill="#3D3C40""
+with "fill=var(--center-channel-text)"
+
+replace "fill="#FF8800""
+with "fill=var(--new-message-separator)"
+
+replace "fill="#FD5960""
+with "fill=var(--error-text)"
+
+replace "fill="#FFE577""
+with "fill=var(--mention-highlight-bg)"
+
+replace "fill="#166DE0""
+with "fill=var(--button-bg)"
+
+replace "fill="#2389D7""
+with "fill=var(--link-color)"
