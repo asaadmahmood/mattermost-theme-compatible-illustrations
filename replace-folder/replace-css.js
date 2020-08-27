880dd67a57c0f447f338a2712f0e9bdfb9b21748
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-08-27 18:20:26 +0500

    Updating css

--- replace-folder/replace-css.js
+++ replace-folder/replace-css.js
@@ -1,36 +1,36 @@
 filter "*.svg"
 replace "fill="#145DBF""
-with "fill=var(--sidebar-bg)"
+with "fill='var(--sidebar-bg)'"
 
 replace "fill="white""
-with "fill=var(--center-channel-bg)"
+with "fill='var(--center-channel-bg)'"
 
 replace "fill="#1153AB""
-with "fill=var(--sidebar-header-bg)"
+with "fill='var(--sidebar-header-bg)'"
 
 replace "fill="#06D6A0""
-with "fill=var(--online-indicator)"
+with "fill='var(--online-indicator)'"
 
 replace "fill="#FFBC42""
-with "fill=var(--away-indicator)"
+with "fill='var(--away-indicator)'"
 
 replace "fill="#F74343""
-with "fill=var(--dnd-indicator)"
+with "fill='var(--dnd-indicator)'"
 
 replace "fill="#3D3C40""
-with "fill=var(--center-channel-text)"
+with "fill='var(--center-channel-text)'"
 
 replace "fill="#FF8800""
-with "fill=var(--new-message-separator)"
+with "fill='var(--new-message-separator)'"
 
 replace "fill="#FD5960""
-with "fill=var(--error-text)"
+with "fill='var(--error-text)'"
 
 replace "fill="#FFE577""
-with "fill=var(--mention-highlight-bg)"
+with "fill='var(--mention-highlight-bg)'"
 
 replace "fill="#166DE0""
-with "fill=var(--button-bg)"
+with "fill='var(--button-bg)'"
 
 replace "fill="#2389D7""
-with "fill=var(--link-color)"
+with "fill='var(--link-color)'"
